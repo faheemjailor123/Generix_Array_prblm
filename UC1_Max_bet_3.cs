f8fb24d57c7261234d82bcba43d7733e8ed7fed8
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-10-25 05:21:21 -0700

    UC1_max_bet_three.cs

---
+++ UC1_Max_bet_3.cs
@@ -0,0 +1,22 @@
+public static int MaximumIntegerNumber(int firstValue, int secondValue, thirdValue)
+{ 
+ if(firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue))  >  0 ||
+     firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue))  >  0 ||
+     firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue))  >=  0 )
+ {
+    return firstvalue;
+ }  
+ if (secondValue.CompareTo(firstValue) > 0  &&  secondValue.CompareTo(thirdValue) > 0  ||
+      secondValue.CompareTo(firstValue) >= 0  &&  secondValue.CompareTo(thirdValue) > 0  ||
+      secondValue.CompareTo(firstValue) > 0  &&  secondValue.CompareTo(thirdValue) >= 0 )
+ {
+       return.secondvalue;
+ }  
+ if  (thirdvalue.CompareTo(firstValue) > 0 &&  thirdvalue.CompareTo(secondValue) > 0    ||
+      thirdvalue.CompareTo(firstValue) > =0 &&  thirdvalue.CompareTo(secondValue) > 0    ||
+      thirdvalue.CompareTo(firstValue) > 0 &&  thirdvalue.CompareTo(secondValue) > =0)  
+ {
+       return.thirdvalue;
+ }     
+ return firstvalue
+ }    
